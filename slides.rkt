1868b31203242cdd773fb69afcf98c251ef31240
Petey <king.petey@sbcglobal.net>
2011-01-21 13:26:42 -0700

    almost alpha

--- slides.rkt
+++ slides.rkt
@@ -2,11 +2,14 @@
 
 (require slideshow/code)
 
-(slide #:title "What is Racket? (vs. Scheme)")
+(slide #:title "Racket - a crash course")
+
+(slide #:title "What is Racket?"
+       (t "Scheme with useful libraries"))
 (slide #:title "DrRacket howto")
 (slide #:title "Command-line Racket howto"
-       (item "Add to $PATH if necessary")
-       (item "Why the errors?"))
+       #;(item "Add to $PATH if necessary")
+       #;(item "Why the errors?"))
 (slide #:title "Scheme Basics"
        'next
        (para "s-expressions:")
@@ -26,17 +29,35 @@
        (para "Lists:"
              (item (tt "empty"))
              (item "A pair of a value and a list")))
-(slide #:title "Scheme Basics: constructing lists"
-       (item (tt "(cons") (it "value list") (tt ")"))
-       (item (tt "(list") (it "value ...") (tt ")")))
-(slide #:title "Scheme Basics: define")
-(slide #:title "Scheme Basics: lambda")
-(slide #:title "Scheme Basics: let")
+(slide #:title "Scheme Basics"
+       (para "List construction:"
+             (item (tt "(cons") (it "value list") (tt ")"))
+             (item (tt "(list") (it "value") (tt "...)"))))
+(slide #:title "Scheme Basics"
+       (para "Function definition:")
+       (item (tt "(λ (")
+             (it "formal")
+             (tt "...)")
+             (it "body-expr")
+             (tt "...)"))
+       (item (tt "(lambda (")
+             (it "formal")
+             (tt "...)")
+             (it "body-expr")
+             (tt "...)")))
+(slide #:title "Scheme Basics"
+       (item "define")
+       (item "let"))
 (slide #:title "Putting it all together"
        (item ""))
-(slide #:title "module system")
+(slide #:title "List operations"
+       (item (tt "(map)"))
+       (item (tt "(foldl)")))
+(slide #:title "Module System"
+       ; maybe show racket doc to demonstrate how to know what to import
+       (para (tt "(require") (it "module") (tt ")")))
 (slide #:title "quasiquotes")
-(slide #:title "read procedure")
+(slide #:title "(read)")
 (slide #:title "match construct")
 (slide #:title "regular expressions")
 (slide #:title "structs")
